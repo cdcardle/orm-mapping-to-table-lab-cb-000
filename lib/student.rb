3e787a4c4fcd3c6ea98ed7767153c7e15ebc1658
Chris Cardle <cdcardle@gmail.com>
2018-01-18 17:11:20 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,7 +6,19 @@ class Student
     self.name = name
     self.grade = grade
   end
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
 
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+      )
+      SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    
+  end
 end
