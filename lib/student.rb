ec060244fd1986829b1371fb1fc0a7650e39baff
Chris Cardle <cdcardle@gmail.com>
2018-01-18 17:19:10 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,6 +32,6 @@ class Student
       SQL
     DB[:conn].execute(sql, self.name, self.grade)
 
-    self.id = DB[:conn].execute("SELECT id FROM students ORDER BY id DESC LIMIT 1;")[0][0]
+    @id = DB[:conn].execute("SELECT id FROM students ORDER BY id DESC LIMIT 1;")[0][0]
   end
 end
